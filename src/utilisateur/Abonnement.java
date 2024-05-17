51eec3867aa820b26be947f8f40231222084f218
Hugo Pereira <54220880+tigrou23@users.noreply.github.com>
2024-05-17 13:52:50 +0200

    gestion et calcul de la date de fin de l'abonnement

--- src/utilisateur/Abonnement.java
+++ src/utilisateur/Abonnement.java
@@ -6,11 +6,12 @@ public class Abonnement {
     private TYPE_ABONNEMENT type;
     private Date dateDebut;
     private Date dateFin;
+    private static final int DUREE_ABONNEMENT = 30;
 
-    public Abonnement(TYPE_ABONNEMENT type, Date dateDebut, Date dateFin) {
+    public Abonnement(TYPE_ABONNEMENT type, Date dateDebut) {
         this.type = type;
         this.dateDebut = dateDebut;
-        this.dateFin = dateFin;
+        this.dateFin = new Date(dateDebut.getTime() + (long) DUREE_ABONNEMENT * 24 * 60 * 60 * 1000);
     }
 
     public TYPE_ABONNEMENT getType() {
